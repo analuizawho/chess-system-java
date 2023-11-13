d05f514a81a2fdc971e10a7daedc52ff89fecf02
Ana Luiza <analuiza319@hotmail.com>
2023-11-13 10:15:33 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
+import boardgame.Position;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args){
-        
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
